95fa5a89a1153099229a1bc8678d492f3463b193
Broccolingual <broccolingual@gmail.com>
2023-05-08 15:14:33 +0900

    add: update tarkov price command

--- cogs/tarkovMarket.py
+++ cogs/tarkovMarket.py
@@ -1,9 +1,68 @@
 import datetime
-import settings
 
 import discord
 from discord.ext import commands
 import requests
+
+class TarkovPriceView(discord.ui.View):
+    def __init__(self, bot, result, timeout=300): # timeout - 5m
+        super().__init__(timeout=timeout)
+        self.bot = bot
+        self.result = result
+        self.resultIndex = 0
+        self.resultMax = len(result)
+
+    async def disable_all_items(self):
+        for item in self.children:
+            item.disable = True
+        await self.message.edit(view=self)
+
+    async def on_timeout(self):
+        await self.disable_all_items()
+
+    @discord.ui.button(label="←",
+                       style=discord.ButtonStyle.blurple)
+    async def prevSugesstion(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.resultIndex == 0:
+            self.resultIndex = self.resultMax-1
+        else:
+            self.resultIndex -= 1
+        currentItem = self.result[self.resultIndex]
+        oldEmbed = interaction.message.embeds[0]
+        oldEmbed.description = f"[{self.resultIndex+1}/{self.resultMax}] {currentItem['name']}"
+        oldEmbed.set_thumbnail(url=currentItem["image8xLink"])
+        oldEmbed.set_field_at(0, name="Avg 24h", value=f"**{'{:,}'.format(currentItem['avg24hPrice'])}** Roubles")
+        if currentItem['changeLast48h'] < 0:
+            oldEmbed.set_field_at(1, name="Change last 48h", value=f"**{'{:,}'.format(currentItem['changeLast48h'])}** Roubles :arrow_heading_down:")
+        else:
+            oldEmbed.set_field_at(1, name="Change last 48h", value=f"**{'{:,}'.format(currentItem['changeLast48h'])}** Roubles :arrow_heading_up:")
+        await interaction.message.edit(embeds=[oldEmbed])
+        # 「インタラクションに失敗しました」対策
+        await interaction.response.send_message("")
+
+    @discord.ui.button(label="→",
+                       style=discord.ButtonStyle.success)
+    async def nextSuggestion(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.resultIndex == self.resultMax-1:
+            self.resultIndex = 0
+        else:
+            self.resultIndex += 1
+        try:
+            currentItem = self.result[self.resultIndex]
+            oldEmbed = interaction.message.embeds[0]
+            oldEmbed.description = f"[{self.resultIndex+1}/{self.resultMax}] {currentItem['name']}"
+            oldEmbed.set_thumbnail(url=currentItem["image8xLink"])
+            oldEmbed.set_field_at(0, name="Avg 24h", value=f"**{'{:,}'.format(currentItem['avg24hPrice'])}** Roubles")
+            if currentItem['changeLast48h'] < 0:
+                oldEmbed.set_field_at(1, name="Change last 48h", value=f"**{'{:,}'.format(currentItem['changeLast48h'])}** Roubles :arrow_heading_down:")
+            else:
+                oldEmbed.set_field_at(1, name="Change last 48h", value=f"**{'{:,}'.format(currentItem['changeLast48h'])}** Roubles :arrow_heading_up:")
+            await interaction.message.edit(embeds=[oldEmbed])
+            # 「インタラクションに失敗しました」対策
+            await interaction.response.send_message("")
+        except Exception as e:
+            print(e)
+
 
 def getPriceFromKeyword(keyword):
     query = f"""
@@ -15,6 +74,7 @@ query {{
         avg24hPrice
         changeLast48h
         image8xLink
+        types
         updated
     }}
 }}
@@ -44,19 +104,29 @@ class Tarkov(commands.Cog):
         keyword = " ".join(keywords)
         if keyword == "":
             await ctx.author.send(f"{ctx.author.mention}価格を調べるアイテムのキーワードが必要です。")
+            return
         resp = getPriceFromKeyword(keyword)
         if resp is None:
             await ctx.author.send(f"{ctx.author.mention}エラーが発生しました。少し時間を置いてお試しください。")
-        if resp["data"]["items"] == []:
+            return
+        searchItems = resp["data"]["items"]
+        if len(searchItems) == 0:
             await ctx.author.send(f"{ctx.author.mention}アイテムが見つかりませんでした。")
-        topItem = resp["data"]["items"][0]
-        embed = discord.Embed(title=topItem["name"], color=discord.Colour.random())
-        embed.set_thumbnail(url=topItem["image8xLink"])
+            return
+        
+        priceView = TarkovPriceView(self.bot, searchItems)
+        embed = discord.Embed(title=f"Search Result ({len(searchItems)} search results)",
+                            description=f"[1/{len(searchItems)}] {searchItems[0]['name']}",
+                            color=discord.Colour.random())
         embed.set_footer(text="Powered by tarkov.dev")
         embed.timestamp = datetime.datetime.now()
-        embed.add_field(name="Avg 24h", value=f"{topItem['avg24hPrice']} Roubles")
-        embed.add_field(name="Change last 48h", value=f"{topItem['changeLast48h']} Roubles")
-        await ctx.author.send(embed=embed)
+        embed.set_thumbnail(url=searchItems[0]["image8xLink"])
+        embed.add_field(name="Avg 24h", value=f"**{'{:,}'.format(searchItems[0]['avg24hPrice'])}** Roubles")
+        if searchItems[0]['changeLast48h'] < 0:
+            embed.add_field(name="Change last 48h", value=f"**{'{:,}'.format(searchItems[0]['changeLast48h'])}** Roubles :arrow_heading_down:")
+        else:
+            embed.add_field(name="Change last 48h", value=f"**{'{:,}'.format(searchItems[0]['changeLast48h'])}** Roubles :arrow_heading_up:")
+        await ctx.author.send(embeds=[embed], view=priceView)
 
 async def setup(bot):
     await bot.add_cog(Tarkov(bot))
