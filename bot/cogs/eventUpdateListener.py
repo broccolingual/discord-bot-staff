17ac26b2afbbfa983bcfe036a735868b35fcc68f
broccolingual <broccolingual@gmail.com>
2025-04-02 17:03:29 +0900

    fix library problem

--- bot/cogs/eventUpdateListener.py
+++ bot/cogs/eventUpdateListener.py
@@ -12,10 +12,12 @@ from cogs.pointManager import PointManager
 
 logger = logging.getLogger("discord").getChild("eventManager")
 
+
 class EventCommentForm(discord.ui.Modal):
-    comment = discord.ui.TextInput(label="Contents", placeholder="I'll be a little late...", style=discord.TextStyle.long)
-    
-    def __init__(self, timeout=86400, origInteraction=None): # timeout - 24h
+    comment = discord.ui.TextInput(
+        label="Contents", placeholder="I'll be a little late...", style=discord.TextStyle.long)
+
+    def __init__(self, timeout=86400, origInteraction=None):  # timeout - 24h
         super().__init__(title="Comment", timeout=timeout)
         self.origInteraction = origInteraction
 
@@ -28,12 +30,13 @@ class EventCommentForm(discord.ui.Modal):
         oldEmbed.set_field_at(4, name=oldEmbed.fields[4].name, value=oldValue)
         await self.origInteraction.message.edit(embeds=[oldEmbed])
         await interaction.response.send_message("Your comment has sended correctly.", ephemeral=True, delete_after=10)
-        
+
     async def on_error(self, interaction: discord.Interaction, e: Exception):
         traceback.print_exception(type(e), e, e.__traceback__)
 
+
 class EventView(discord.ui.View):
-    def __init__(self, bot, event: discord.ScheduledEvent, timeout=86400): # timeout - 24h
+    def __init__(self, bot, event: discord.ScheduledEvent, timeout=86400):  # timeout - 24h
         super().__init__(timeout=timeout)
         self.bot = bot
         self.event = event
@@ -44,7 +47,7 @@ class EventView(discord.ui.View):
 
     async def on_timeout(self):
         await self.disable_all_items()
-    
+
     @discord.ui.button(label="Join",
                        style=discord.ButtonStyle.success)
     async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -58,14 +61,15 @@ class EventView(discord.ui.View):
                 return
             await db.addJoinedUser(self.event.id, interaction.user.id)
             joining_user_ids.append(interaction.user.id)
-            await PointManager.addPoint(interaction.guild.id, interaction.user.id, 2) # Point
+            # Point
+            await PointManager.addPoint(interaction.guild.id, interaction.user.id, 2)
         except Exception as e:
             await interaction.response.send_message("Oops... An error occurred during processing.", ephemeral=True, delete_after=10)
             traceback.print_exception(type(e), e, e.__traceback__)
             return
-        
+
         # update embed
-        oldEmbed = interaction.message.embeds[0] # get old embed
+        oldEmbed = interaction.message.embeds[0]  # get old embed
         newValue = ""
         for i, joining_user_id in enumerate(joining_user_ids):
             user = self.bot.get_user(joining_user_id)
@@ -87,14 +91,15 @@ class EventView(discord.ui.View):
                 return
             await db.deleteJoinedUser(self.event.id, interaction.user.id)
             joining_user_ids.remove(interaction.user.id)
-            await PointManager.removePoint(interaction.guild.id, interaction.user.id, 2) # Point
+            # Point
+            await PointManager.removePoint(interaction.guild.id, interaction.user.id, 2)
         except Exception as e:
             await interaction.response.send_message("Oops... An error occurred during processing.", ephemeral=True, delete_after=10)
             traceback.print_exception(type(e), e, e.__traceback__)
             return
-        
+
         # update embed
-        oldEmbed = interaction.message.embeds[0] # get old embed
+        oldEmbed = interaction.message.embeds[0]  # get old embed
         newValue = ""
         for i, joining_user_id in enumerate(joining_user_ids):
             user = self.bot.get_user(joining_user_id)
@@ -111,35 +116,35 @@ class EventView(discord.ui.View):
         except Exception as e:
             await interaction.response.send_message("Oops... An error occurred during processing.", ephemeral=True, delete_after=10)
             print(e)
-            
+
     @discord.ui.button(label="Start event",
                        style=discord.ButtonStyle.gray)
     async def start(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.event is None:
             await interaction.response.send_message("Failed to retrieve event.", ephemeral=True, delete_after=10)
             return
-        
+
         if self.event.creator != interaction.user:
             await interaction.response.send_message("You are not the creator of this event.", ephemeral=True, delete_after=10)
             return
-        
+
         if self.event.status == discord.EventStatus.scheduled:
             await self.event.start()
             await interaction.response.send_message("Event has started!", ephemeral=True, delete_after=10)
         elif self.event.status == discord.EventStatus.active or self.event.status == discord.EventStatus.ended:
             await interaction.response.send_message("This event is already active.", ephemeral=True, delete_after=10)
-            
+
     @discord.ui.button(label="Cancel/End event",
                        style=discord.ButtonStyle.gray)
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.event is None:
             await interaction.response.send_message("Failed to retrieve event.", ephemeral=True, delete_after=10)
             return
-        
+
         if self.event.creator != interaction.user:
             await interaction.response.send_message("You are not the creator of this event.", ephemeral=True, delete_after=10)
             return
-        
+
         if self.event.status == discord.EventStatus.active:
             await self.event.end()
             await interaction.response.send_message("Event was ended correctly.", ephemeral=True, delete_after=10)
@@ -147,30 +152,40 @@ class EventView(discord.ui.View):
             await self.event.cancel()
             await interaction.response.send_message("Event was canceled correctly.", ephemeral=True, delete_after=10)
 
+
 class EventNotify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_scheduled_event_create(self, e):
+        logger.info(
+            f"Event was created: {e.guild.name=} - {e.name=} - {e.start_time=}")
         try:
             # create & send embed
-            notifyEmbed = discord.Embed(title=e.name, description=e.description, color=discord.Colour.orange())
-            notifyEmbed.set_author(name="Event (Scheduled)", icon_url=e.guild.icon.url)
+            notifyEmbed = discord.Embed(
+                title=e.name, description=e.description, color=discord.Colour.orange())
+            notifyEmbed.set_author(
+                name="Event (Scheduled)", icon_url=e.guild.icon.url)
             notifyEmbed.add_field(name="🕒 Schedule",
                                   value=f"**{e.start_time.astimezone(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}~**")
             if e.location is not None:
-                notifyEmbed.add_field(name="📍 Location", value=f"**{e.location}**")
+                notifyEmbed.add_field(
+                    name="📍 Location", value=f"**{e.location}**")
             else:
-                notifyEmbed.add_field(name="📡 Channel", value=e.channel.mention)
+                notifyEmbed.add_field(
+                    name="📡 Channel", value=e.channel.mention)
             notifyEmbed.add_field(name="🔗 Event link", value=e.url)
-            notifyEmbed.add_field(name="👥 Applicants", value=f"`1.` {e.creator.mention}")
+            notifyEmbed.add_field(name="👥 Applicants",
+                                  value=f"`1.` {e.creator.mention}")
             notifyEmbed.add_field(name="💬 Comments", value="")
-            notifyEmbed.set_footer(text=f"Event was created by {e.creator.display_name}", icon_url=e.creator.avatar.url)
-            notifyEmbed.set_thumbnail(url=e.cover_image.url if e.cover_image is not None else e.creator.avatar.url)
+            notifyEmbed.set_footer(
+                text=f"Event was created by {e.creator.display_name}", icon_url=e.creator.avatar.url)
+            notifyEmbed.set_thumbnail(
+                url=e.cover_image.url if e.cover_image is not None else e.creator.avatar.url)
             notifyEmbed.timestamp = datetime.datetime.now()
-            notifyView = EventView(self.bot, e, timeout=86400) # timeout - 24h
-            
+            notifyView = EventView(self.bot, e, timeout=86400)  # timeout - 24h
+
             # database
             channel = await db.getEventNotifyChannel(e.guild.id)
             if channel is None:
@@ -179,10 +194,10 @@ class EventNotify(commands.Cog):
             embed = await notifyChan.send(embeds=[notifyEmbed], view=notifyView)
             await db.addEvent(embed.id, e.id, e.creator.id)
             await db.addJoinedUser(e.id, e.creator.id)
-            await PointManager.addPoint(e.guild.id, e.creator.id, 5) # Point
+            await PointManager.addPoint(e.guild.id, e.creator.id, 5)  # Point
         except Exception as err:
             print(err)
-            
+
     @commands.Cog.listener()
     async def on_scheduled_event_update(self, before, after):
         try:
@@ -190,33 +205,51 @@ class EventNotify(commands.Cog):
             notifyChanData = await db.getEventNotifyChannel(before.guild.id)
             if notifyChanData is None:
                 return
-            notifyChan = self.bot.get_partial_messageable(notifyChanData.channel_id)
+            notifyChan = self.bot.get_partial_messageable(
+                notifyChanData.channel_id)
             notifyMsgData = await db.getMessage(before.id)
             if notifyMsgData is None:
                 return
             notifyMsg = await notifyChan.fetch_message(notifyMsgData.msg_id)
             oldEmbed = notifyMsg.embeds[0]
             if after.status == discord.EventStatus.active:
-                newEmbed = discord.Embed(title=after.name, description=after.description, color=discord.Colour.brand_green())
-                newEmbed.set_author(name="Event (Ongoing)", icon_url=after.guild.icon.url)
+                newEmbed = discord.Embed(
+                    title=after.name, description=after.description, color=discord.Colour.brand_green())
+                newEmbed.set_author(name="Event (Ongoing)",
+                                    icon_url=after.guild.icon.url)
+                logger.info(
+                    f"Event was started: {after.guild.name=} - {after.name=} - {after.start_time=}")
             elif after.status == discord.EventStatus.ended or after.status == discord.EventStatus.cancelled:
-                newEmbed = discord.Embed(title=after.name, description=after.description, color=discord.Colour.brand_red())
-                newEmbed.set_author(name="Event (Inactive)", icon_url=after.guild.icon.url)
+                newEmbed = discord.Embed(
+                    title=after.name, description=after.description, color=discord.Colour.brand_red())
+                newEmbed.set_author(name="Event (Inactive)",
+                                    icon_url=after.guild.icon.url)
+                logger.info(
+                    f"Event was ended: {after.guild.name=} - {after.name=} - {after.start_time=}")
             else:
-                newEmbed = discord.Embed(title=after.name, description=after.description, color=discord.Colour.orange())
-                newEmbed.set_author(name="Event (Scheduled)", icon_url=after.guild.icon.url)
-            newEmbed.set_thumbnail(url=after.cover_image.url if after.cover_image is not None else after.creator.avatar.url)
+                newEmbed = discord.Embed(
+                    title=after.name, description=after.description, color=discord.Colour.orange())
+                newEmbed.set_author(name="Event (Scheduled)",
+                                    icon_url=after.guild.icon.url)
+                logger.info(
+                    f"Event was updated: {after.guild.name=} - {after.name=} - {after.start_time=}")
+            newEmbed.set_thumbnail(
+                url=after.cover_image.url if after.cover_image is not None else after.creator.avatar.url)
             for i, field in enumerate(oldEmbed.fields):
-                if i == 0: # Schedule
-                    newEmbed.add_field(name=field.name, value=f"**{after.start_time.astimezone(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}~**")
-                elif i == 1: # Location or Channel
+                if i == 0:  # Schedule
+                    newEmbed.add_field(
+                        name=field.name, value=f"**{after.start_time.astimezone(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}~**")
+                elif i == 1:  # Location or Channel
                     if after.location is not None:
-                        newEmbed.add_field(name="📍 Location", value=f"**{after.location}**")
+                        newEmbed.add_field(
+                            name="📍 Location", value=f"**{after.location}**")
                     else:
-                        newEmbed.add_field(name="📡 Channel", value=after.channel.mention)
+                        newEmbed.add_field(
+                            name="📡 Channel", value=after.channel.mention)
                 else:
                     newEmbed.add_field(name=field.name, value=field.value)
-            newEmbed.set_footer(text=oldEmbed.footer.text, icon_url=oldEmbed.footer.icon_url)
+            newEmbed.set_footer(text=oldEmbed.footer.text,
+                                icon_url=oldEmbed.footer.icon_url)
             newEmbed.timestamp = oldEmbed.timestamp
             if after.status == discord.EventStatus.ended or after.status == discord.EventStatus.cancelled:
                 await notifyMsg.edit(embeds=[newEmbed], view=None)
@@ -224,19 +257,20 @@ class EventNotify(commands.Cog):
                 await notifyMsg.edit(embeds=[newEmbed])
         except Exception as err:
             print(err)
-            
+
+
 class EventNotifyChannelResistrationView(discord.ui.View):
     def __init__(self, timeout=60):
         super().__init__(timeout=timeout)
         self.channel = None
-        
+
     async def disable_all_items(self):
         for item in self.children:
             item.disable = True
-        
+
     async def on_timeout(self):
         await self.disable_all_items()
-    
+
     @discord.ui.select(
         placeholder="Select a channel to notify",
         cls=discord.ui.ChannelSelect,
@@ -244,8 +278,8 @@ class EventNotifyChannelResistrationView(discord.ui.View):
     )
     async def select_channel(self, interaction: discord.Interaction, channel: discord.TextChannel):
         self.channel = channel
-        await blank_interaction(interaction) # 「インタラクションに失敗しました」対策
-    
+        await blank_interaction(interaction)  # 「インタラクションに失敗しました」対策
+
     @discord.ui.button(label="Register",
                        style=discord.ButtonStyle.success)
     async def register(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -264,20 +298,24 @@ class EventNotifyChannelResistrationView(discord.ui.View):
         except Exception as err:
             await interaction.response.edit_message(content="Notify channel is not updated by some reason.", view=None)
             print(err)
-            
-class EventNotifyChannelResister(app_commands.Group):            
+
+
+class EventNotifyChannelResister(app_commands.Group):
     @app_commands.command(
         name="resister",
         description="Register the channel to notify (only for administrators)",
     )
     @app_commands.checks.has_permissions(administrator=True)
     async def register(self, interaction: discord.Interaction):
-        logger.info(f"command: 'notify resister' executed by {interaction.user.name}")
+        logger.info(
+            f"command: 'notify resister' executed by {interaction.user.name}")
         try:
             await interaction.response.send_message("Please click the button to register the channel.", view=EventNotifyChannelResistrationView())
         except Exception as err:
             print(err)
 
+
 async def setup(bot):
     await bot.add_cog(EventNotify(bot))
-    bot.tree.add_command(EventNotifyChannelResister(name="notify", description="Notify channel registration"))
+    bot.tree.add_command(EventNotifyChannelResister(
+        name="notify", description="Notify channel registration"))
