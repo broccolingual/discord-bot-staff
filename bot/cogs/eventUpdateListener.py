b604e8a94808e4374b2e098ff882de554b7b09bf
broccolingual <broccolingual@gmail.com>
2025-04-03 20:05:07 +0900

    refactor

--- bot/cogs/eventUpdateListener.py
+++ bot/cogs/eventUpdateListener.py
@@ -61,7 +61,6 @@ class EventView(discord.ui.View):
                 return
             await db.addJoinedUser(self.event.id, interaction.user.id)
             joining_user_ids.append(interaction.user.id)
-            # Point
             await PointManager.addPoint(interaction.guild.id, interaction.user.id, 2)
         except Exception as e:
             await interaction.response.send_message("Oops... An error occurred during processing.", ephemeral=True, delete_after=10)
@@ -91,11 +90,9 @@ class EventView(discord.ui.View):
                 return
             await db.deleteJoinedUser(self.event.id, interaction.user.id)
             joining_user_ids.remove(interaction.user.id)
-            # Point
             await PointManager.removePoint(interaction.guild.id, interaction.user.id, 2)
         except Exception as e:
             await interaction.response.send_message("Oops... An error occurred during processing.", ephemeral=True, delete_after=10)
-            traceback.print_exception(type(e), e, e.__traceback__)
             return
 
         # update embed
@@ -113,9 +110,8 @@ class EventView(discord.ui.View):
     async def comment(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
             await interaction.response.send_modal(EventCommentForm(timeout=86400, origInteraction=interaction))
-        except Exception as e:
+        except Exception:
             await interaction.response.send_message("Oops... An error occurred during processing.", ephemeral=True, delete_after=10)
-            print(e)
 
     @discord.ui.button(label="Start event",
                        style=discord.ButtonStyle.gray)
@@ -161,102 +157,134 @@ class EventNotify(commands.Cog):
     async def on_scheduled_event_create(self, e):
         logger.info(
             f"Event was created: {e.guild.name=} - {e.name=} - {e.start_time=}")
-        try:
-            # create & send embed
-            notifyEmbed = discord.Embed(
-                title=e.name, description=e.description, color=discord.Colour.orange())
-            notifyEmbed.set_author(
-                name="Event (Scheduled)", icon_url=e.guild.icon.url)
-            notifyEmbed.add_field(name="🕒 Schedule",
-                                  value=f"**{e.start_time.astimezone(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}~**")
-            if e.location is not None:
-                notifyEmbed.add_field(
-                    name="📍 Location", value=f"**{e.location}**")
-            else:
-                notifyEmbed.add_field(
-                    name="📡 Channel", value=e.channel.mention)
-            notifyEmbed.add_field(name="🔗 Event link", value=e.url)
-            notifyEmbed.add_field(name="👥 Applicants",
-                                  value=f"`1.` {e.creator.mention}")
-            notifyEmbed.add_field(name="💬 Comments", value="")
-            notifyEmbed.set_footer(
-                text=f"Event was created by {e.creator.display_name}", icon_url=e.creator.avatar.url)
-            notifyEmbed.set_thumbnail(
-                url=e.cover_image.url if e.cover_image is not None else e.creator.avatar.url)
-            notifyEmbed.timestamp = datetime.datetime.now()
-            notifyView = EventView(self.bot, e, timeout=86400)  # timeout - 24h
 
-            # database
+        # create & send embed
+        notifyEmbed = discord.Embed(
+            title=e.name, description=e.description, color=discord.Colour.orange())
+        notifyEmbed.set_author(
+            name="Event (Scheduled)", icon_url=e.guild.icon.url)
+        notifyEmbed.add_field(name="🕒 Schedule",
+                              value=f"**{e.start_time.astimezone(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}~**")
+        if e.location is not None:
+            notifyEmbed.add_field(
+                name="📍 Location", value=f"**{e.location}**")
+        else:
+            notifyEmbed.add_field(
+                name="📡 Channel", value=e.channel.mention)
+        notifyEmbed.add_field(name="🔗 Event link", value=e.url)
+        notifyEmbed.add_field(name="👥 Applicants",
+                              value=f"`1.` {e.creator.mention}")
+        notifyEmbed.add_field(name="💬 Comments", value="")
+        notifyEmbed.set_footer(
+            text=f"Event was created by {e.creator.display_name}", icon_url=e.creator.avatar.url)
+        notifyEmbed.set_thumbnail(
+            url=e.cover_image.url if e.cover_image is not None else e.creator.avatar.url)
+        notifyEmbed.timestamp = datetime.datetime.now()
+        notifyView = EventView(self.bot, e, timeout=86400)  # timeout - 24h
+
+        try:
             channel = await db.getEventNotifyChannel(e.guild.id)
-            if channel is None:
-                return
-            notifyChan = self.bot.get_partial_messageable(channel.channel_id)
-            embed = await notifyChan.send(embeds=[notifyEmbed], view=notifyView)
+        except Exception:
+            logger.error(
+                f"Failed to get notify channel: {e.guild.name=}")
+            return
+
+        if channel is None:
+            logger.warning(
+                f"Notify channel is not registered: {e.guild.name=}")
+            return
+
+        notifyChan = self.bot.get_partial_messageable(channel.channel_id)
+        embed = await notifyChan.send(embeds=[notifyEmbed], view=notifyView)
+
+        try:
             await db.addEvent(embed.id, e.id, e.creator.id)
             await db.addJoinedUser(e.id, e.creator.id)
             await PointManager.addPoint(e.guild.id, e.creator.id, 5)  # Point
-        except Exception as err:
-            print(err)
+        except Exception:
+            logger.error(
+                f"Failed to add notify message: {e.guild.name=}")
+            return
 
     @commands.Cog.listener()
     async def on_scheduled_event_update(self, before, after):
         try:
-            # database
             notifyChanData = await db.getEventNotifyChannel(before.guild.id)
-            if notifyChanData is None:
-                return
-            notifyChan = self.bot.get_partial_messageable(
-                notifyChanData.channel_id)
+        except Exception:
+            logger.error(
+                f"Failed to get notify channel: {before.guild.name=}")
+            return
+
+        if notifyChanData is None:
+            logger.warning(
+                f"Notify channel is not registered: {before.guild.name=}")
+            return
+
+        notifyChan = self.bot.get_partial_messageable(
+            notifyChanData.channel_id)
+
+        try:
             notifyMsgData = await db.getMessage(before.id)
-            if notifyMsgData is None:
-                return
-            notifyMsg = await notifyChan.fetch_message(notifyMsgData.msg_id)
-            oldEmbed = notifyMsg.embeds[0]
-            if after.status == discord.EventStatus.active:
-                newEmbed = discord.Embed(
-                    title=after.name, description=after.description, color=discord.Colour.brand_green())
-                newEmbed.set_author(name="Event (Ongoing)",
-                                    icon_url=after.guild.icon.url)
-                logger.info(
-                    f"Event was started: {after.guild.name=} - {after.name=} - {after.start_time=}")
-            elif after.status == discord.EventStatus.ended or after.status == discord.EventStatus.cancelled:
-                newEmbed = discord.Embed(
-                    title=after.name, description=after.description, color=discord.Colour.brand_red())
-                newEmbed.set_author(name="Event (Inactive)",
-                                    icon_url=after.guild.icon.url)
-                logger.info(
-                    f"Event was ended: {after.guild.name=} - {after.name=} - {after.start_time=}")
-            else:
-                newEmbed = discord.Embed(
-                    title=after.name, description=after.description, color=discord.Colour.orange())
-                newEmbed.set_author(name="Event (Scheduled)",
-                                    icon_url=after.guild.icon.url)
-                logger.info(
-                    f"Event was updated: {after.guild.name=} - {after.name=} - {after.start_time=}")
-            newEmbed.set_thumbnail(
-                url=after.cover_image.url if after.cover_image is not None else after.creator.avatar.url)
-            for i, field in enumerate(oldEmbed.fields):
-                if i == 0:  # Schedule
+        except Exception:
+            logger.error(
+                f"Failed to get notify message: {before.guild.name=}")
+            return
+
+        if notifyMsgData is None:
+            logger.warning(
+                f"Notify message is not registered: {before.guild.name=}")
+            return
+
+        notifyMsg = await notifyChan.fetch_message(notifyMsgData.msg_id)
+        oldEmbed = notifyMsg.embeds[0]
+
+        if after.status == discord.EventStatus.active:
+            newEmbed = discord.Embed(
+                title=after.name, description=after.description, color=discord.Colour.brand_green())
+            newEmbed.set_author(name="Event (Ongoing)",
+                                icon_url=after.guild.icon.url)
+            logger.info(
+                f"Event was started: {after.guild.name=} - {after.name=} - {after.start_time=}")
+        elif after.status == discord.EventStatus.ended or after.status == discord.EventStatus.cancelled:
+            newEmbed = discord.Embed(
+                title=after.name, description=after.description, color=discord.Colour.brand_red())
+            newEmbed.set_author(name="Event (Inactive)",
+                                icon_url=after.guild.icon.url)
+            logger.info(
+                f"Event was ended: {after.guild.name=} - {after.name=} - {after.start_time=}")
+        else:
+            newEmbed = discord.Embed(
+                title=after.name, description=after.description, color=discord.Colour.orange())
+            newEmbed.set_author(name="Event (Scheduled)",
+                                icon_url=after.guild.icon.url)
+            logger.info(
+                f"Event was updated: {after.guild.name=} - {after.name=} - {after.start_time=}")
+
+        newEmbed.set_thumbnail(
+            url=after.cover_image.url if after.cover_image is not None else after.creator.avatar.url)
+
+        for i, field in enumerate(oldEmbed.fields):
+            if i == 0:  # Schedule
+                newEmbed.add_field(
+                    name=field.name, value=f"**{after.start_time.astimezone(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}~**")
+            elif i == 1:  # Location or Channel
+                if after.location is not None:
                     newEmbed.add_field(
-                        name=field.name, value=f"**{after.start_time.astimezone(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y-%m-%d %H:%M')}~**")
-                elif i == 1:  # Location or Channel
-                    if after.location is not None:
-                        newEmbed.add_field(
-                            name="📍 Location", value=f"**{after.location}**")
-                    else:
-                        newEmbed.add_field(
-                            name="📡 Channel", value=after.channel.mention)
+                        name="📍 Location", value=f"**{after.location}**")
                 else:
-                    newEmbed.add_field(name=field.name, value=field.value)
-            newEmbed.set_footer(text=oldEmbed.footer.text,
-                                icon_url=oldEmbed.footer.icon_url)
-            newEmbed.timestamp = oldEmbed.timestamp
-            if after.status == discord.EventStatus.ended or after.status == discord.EventStatus.cancelled:
-                await notifyMsg.edit(embeds=[newEmbed], view=None)
+                    newEmbed.add_field(
+                        name="📡 Channel", value=after.channel.mention)
             else:
-                await notifyMsg.edit(embeds=[newEmbed])
-        except Exception as err:
-            print(err)
+                newEmbed.add_field(name=field.name, value=field.value)
+
+        newEmbed.set_footer(text=oldEmbed.footer.text,
+                            icon_url=oldEmbed.footer.icon_url)
+        newEmbed.timestamp = oldEmbed.timestamp
+
+        if after.status == discord.EventStatus.ended or after.status == discord.EventStatus.cancelled:
+            await notifyMsg.edit(embeds=[newEmbed], view=None)
+        else:
+            await notifyMsg.edit(embeds=[newEmbed])
 
 
 class EventNotifyChannelResistrationView(discord.ui.View):
@@ -283,21 +311,33 @@ class EventNotifyChannelResistrationView(discord.ui.View):
     @discord.ui.button(label="Register",
                        style=discord.ButtonStyle.success)
     async def register(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.channel is None:
+            await interaction.response.send_message("Please select a channel.")
+            return
+
         try:
-            if self.channel is None:
-                await interaction.response.send_message("Please select a channel.")
-                return
-            # database
             registeredId = await db.getEventNotifyChannel(interaction.guild.id)
-            if registeredId is None:
+        except Exception:
+            logger.error(
+                f"Failed to get notify channel: {interaction.guild.name=}")
+            return
+
+        if registeredId is None:
+            try:
                 await db.addEventNotifyChannel(interaction.guild.id, self.channel.values[0].id)
                 await interaction.response.edit_message(content="Notify channel is registered.", view=None)
-            else:
+            except Exception:
+                logger.error(
+                    f"Failed to add notify channel: {interaction.guild.name=}")
+                return
+        else:
+            try:
                 await db.updateEventNotifyChannel(interaction.guild.id, self.channel.values[0].id)
                 await interaction.response.edit_message(content="Notify channel is updated.", view=None)
-        except Exception as err:
-            await interaction.response.edit_message(content="Notify channel is not updated by some reason.", view=None)
-            print(err)
+            except Exception:
+                logger.error(
+                    f"Failed to update notify channel: {interaction.guild.name=}")
+                return
 
 
 class EventNotifyChannelResister(app_commands.Group):
@@ -307,12 +347,7 @@ class EventNotifyChannelResister(app_commands.Group):
     )
     @app_commands.checks.has_permissions(administrator=True)
     async def register(self, interaction: discord.Interaction):
-        logger.info(
-            f"command: 'notify resister' executed by {interaction.user.name}")
-        try:
-            await interaction.response.send_message("Please click the button to register the channel.", view=EventNotifyChannelResistrationView())
-        except Exception as err:
-            print(err)
+        await interaction.response.send_message("Please click the button to register the channel.", view=EventNotifyChannelResistrationView())
 
     @register.error
     async def register_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
