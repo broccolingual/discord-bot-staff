2c07e3cb7e0d04c9772459cc3f369f3f8856c37d
broccolingual <broccolingual@gmail.com>
2025-05-23 10:28:51 +0900

    fix: fix typo

--- bot/settings.py
+++ bot/settings.py
@@ -8,6 +8,6 @@ MYSQL_TCP_PORT = os.environ.get('MYSQL_TCP_PORT')
 MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
 DB_DSN = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}@mysql:{MYSQL_TCP_PORT}/{MYSQL_DATABASE}"
 
-SUPABASE_DB_DSN = f"postgresql+asyncpg://{os.environ.get('SUPABASE_DB_USER')}:{os.environ.get('SUPABASE_DB_PASSWORD')}@{os.environ.get('SUPABASE_DB_HOST')}:{os.environ.get("SUPABASE_DB_PORT")}/{os.envirion.get("SUPABASE_DB_NAME")}"
+SUPABASE_DB_DSN = f"postgresql+asyncpg://{os.environ.get('SUPABASE_DB_USER')}:{os.environ.get('SUPABASE_DB_PASSWORD')}@{os.environ.get('SUPABASE_DB_HOST')}:{os.environ.get("SUPABASE_DB_PORT")}/{os.environ.get("SUPABASE_DB_NAME")}"
 SUPABASE_API_URL = os.environ.get('SUPABASE_API_URL')
 SUPABASE_API_KEY = os.environ.get('SUPABASE_API_KEY')
